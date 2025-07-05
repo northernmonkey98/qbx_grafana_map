43f417fdfe4912499bf496d71e5b1e3f49804254
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2025-07-05 21:48:57 +0200

    fix(server): gameToMap accuracy
    The world map is fairly big on the Y axis. However this needs to be spread over a latitude of -90 to 90. This means that at the extreme edge points could be skewed and warped. This now splits the map up in different regions and uses different transforms for each

--- server.lua
+++ server.lua
@@ -1,10 +1,25 @@
 -- smart math inspired by https://github.com/Coffeelot/cw-racingapp/blob/main/html/src/helpers/gameToMap.ts
-local longitudeTransforms = { a = -4.38407057047e-09, b = 0.019935666257721, c = -97.224922143314 }
-local latitudeTransforms = { a = 2.4480481560015e-14, b = -2.6058899468842e-10, c = -7.0754974317837e-07, d = 0.019323267489522, e = 11.908728964674 }
+local transforms = {
+    band1 = { longitude = {a = -3.03663255576895e-09, b = 0.0200175350440883, c = -97.211789757094}, latitude = {a = 6.23122700084464e-14, b = 4.20129073856139e-10, c = 2.33168021020044e-06, d = 0.0240131069198339, e = 14.1346597090657} },
+    band2 = { longitude = {a = 2.04927570146569e-08, b = 0.0199382651698616, c = -97.2328189248356}, latitude = {a = -5.48114969721267e-14, b = -1.7644651413213e-10, c = -6.69398961485353e-07, d = 0.019446455419011, e = 11.948776738614} },
+    band3 = { longitude = {a = 1.14491978248603e-08, b = 0.0199665146367798, c = -97.2865999889282}, latitude = {a = -7.50970630543969e-15, b = 2.42587620752238e-10, c = -3.39800162545523e-06, d = 0.02514652658967, e = 7.49474095495398} },
+    band4 = { longitude = {a = -4.51344244634536e-08, b = 0.0200135106227036, c = -97.1355799536373}, latitude = {a = -2.33545837309061e-14, b = 6.7194107532233e-10, c = -7.73181068541832e-06, d = 0.0443322941450463, e = -23.9182574396233} },
+}
 
 local function gameToMap(x, y)
-    local lng = longitudeTransforms.a * x ^ 2 + longitudeTransforms.b * x + longitudeTransforms.c
-    local lat = latitudeTransforms.a * y ^ 4 + latitudeTransforms.b * y ^ 3 + latitudeTransforms.c * y ^ 2 + latitudeTransforms.d * y + latitudeTransforms.e
+    local t
+    if y < -1000 then
+        t = transforms.band1
+    elseif y < 2000 then
+        t = transforms.band2
+    elseif y < 5000 then
+        t = transforms.band3
+    else
+        t = transforms.band4
+    end
+
+    local lng = t.longitude.a * x^2 + t.longitude.b * x + t.longitude.c
+    local lat = t.latitude.a * y^4 + t.latitude.b * y^3 + t.latitude.c * y^2 + t.latitude.d * y + t.latitude.e
     return lng, lat
 end
 
